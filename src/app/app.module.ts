904ff7af1c2a65ee15b561a69f6490d70de4fd3c
Jabir S. Minjibir <jminjibir@swglobal.com>
2019-11-29 17:18:06 +0100

    Header almost completed.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { FixtureComponent } from './fixture/fixture.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FixtureComponent
   ],
   imports: [
     BrowserModule,
