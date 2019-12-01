f4706014c26b88d7ddc19bae27ac32a5664bea3e
Jabir S. Minjibir <sminjibir@gmail.com>
2019-12-01 11:35:05 +0100

    Main page created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { FooterComponent } from './common/footer/footer.component';
-import { FixtureComponent } from './fixture/fixture.component';
+import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
@@ -13,7 +13,7 @@ import { HttpClientModule } from '@angular/common/http';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    FixtureComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
